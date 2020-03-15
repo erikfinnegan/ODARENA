f15b14b4b3c642a31b30aa717e6c6cfdb9add1f1
Dr. Eki <dreki@lime.st>
2020-03-15 20:13:24 +0200

    Update register.blade.php

--- app/resources/views/pages/round/register.blade.php
+++ app/resources/views/pages/round/register.blade.php
@@ -320,7 +320,7 @@
 
                       @if($round->hasStarted())
                       <p>This round has already started and ends {{ $round->end_date->format('l, jS \o\f F Y \a\t H:i') }}.</p>
-                      <p>When you register, you start with 80 protection ticks. Make the most of them. Once you have used them all, you leave protection immediately.</p>
+                      <p>When you register, you start with 84 protection ticks. Make the most of them. Once you have used them all, you leave protection immediately.</p>
                       <p>Regularly scheduled ticks do not count towards your dominion while you are in protection.</p>
                       <p>To help you get going, you will get 1% extra starting resources for every hour since the round started (max +100%).</p>
 
@@ -330,7 +330,7 @@
 
                       @else
                       <p>The round starts on {{ $round->start_date->format('l, jS \o\f F Y \a\t H:i') }}.</p>
-                      <p>When you register, you start with 80 protection ticks. Make the most of them. Once you have used them all, you leave protection immediately.</p>
+                      <p>When you register, you start with 84 protection ticks. Make the most of them. Once you have used them all, you leave protection immediately.</p>
                       <p>Regularly scheduled ticks do not count towards your dominion while you are in protection.</p>
 
                         @if ($discordInviteLink = config('app.discord_invite_link'))
