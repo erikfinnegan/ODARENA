d9376a044d88be731bebd0a56952e801250011f3
Dr. Eki <dreki@lime.st>
2020-03-18 07:25:16 +0200

    Update MiscController.php

--- src/Http/Controllers/Dominion/MiscController.php
+++ src/Http/Controllers/Dominion/MiscController.php
@@ -4,6 +4,11 @@ namespace OpenDominion\Http\Controllers\Dominion;
 
 use LogicException;
 
+# ODA
+use DB;
+use Auth;
+use Log;
+
 // misc functions, probably could use a refactor later
 class MiscController extends AbstractDominionController
 {
@@ -28,4 +33,61 @@ class MiscController extends AbstractDominionController
 
         return redirect()->back();
     }
+
+    public function postDeleteDominion()
+    {
+
+        /*
+        *   Conditions for allowing deleting:
+        *   - The dominion belongs to the logged in user.
+        *   - If the round has started, only allow deleting if protection ticks > 0.
+        *   - If the round hasn't started, always allow.
+        */
+
+        $dominion = $this->getSelectedDominion();
+
+        # Can only delete your own dominion.
+        if($dominion->user_id !== Auth::user()->id)
+        {
+            throw new GameException('You cannot delete other dominions than your own.');
+        }
+
+        # If the round has started, can only delete if protection ticks > 0.
+        if($dominion->round->hasStarted() and $dominion->protection_ticks <= 0)
+        {
+            throw new GameException('You cannot delete your dominion because the round has already started.');
+        }
+
+        # Destroy the dominion.
+        DB::table('active_spells')->where('dominion_id', '=', $dominion->id)->delete();
+        DB::table('active_spells')->where('cast_by_dominion_id', '=', $dominion->id)->delete();
+
+        DB::table('council_posts')->where('dominion_id', '=', $dominion->id)->delete();
+        DB::table('council_threads')->where('dominion_id', '=', $dominion->id)->delete();
+        DB::table('daily_rankings')->where('dominion_id', '=', $dominion->id)->delete();
+        DB::table('dominion_history')->where('dominion_id', '=', $dominion->id)->delete();
+        DB::table('dominion_queue')->where('dominion_id', '=', $dominion->id)->delete();
+        DB::table('dominion_techs')->where('dominion_id', '=', $dominion->id)->delete();
+        DB::table('dominion_tick')->where('dominion_id', '=', $dominion->id)->delete();
+
+        DB::table('game_events')->where('source_id', '=', $dominion->id)->delete();
+        DB::table('game_events')->where('target_id', '=', $dominion->id)->delete();
+
+        DB::table('info_ops')->where('source_dominion_id', '=', $dominion->id)->delete();
+        DB::table('info_ops')->where('target_dominion_id', '=', $dominion->id)->delete();
+
+        DB::table('dominions')->where('id', '=', $dominion->id)->delete();
+
+        Log::info(sprintf(
+            'The dominion %s (ID %s) was deleted by user %s (ID %s).',
+            $dominion->name,
+            $dominion->id,
+            Auth::user()->display_name,
+            Auth::user()->id
+        ));
+
+        return redirect()->back();
+    }
+
+
 }
