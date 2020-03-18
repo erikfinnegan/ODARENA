8acda77aa4c249cc6655ec5d0705ee529867570b
Dr. Eki <dreki@lime.st>
2020-03-18 07:37:08 +0200

    Update ProtectionService.php

--- src/Services/Dominion/ProtectionService.php
+++ src/Services/Dominion/ProtectionService.php
@@ -8,13 +8,44 @@ use OpenDominion\Models\Dominion;
 class ProtectionService
 {
     /**
-     * Returns whether this under protection (has protection_ticks > 0).
+     * Returns whether this dominion is under protection (has protection_ticks > 0).
      *
      * @param Dominion $dominion
      * @return bool
      */
     public function isUnderProtection(Dominion $dominion): bool
     {
-        return $dominion->protection_ticks;
+        return max(0, $dominion->protection_ticks);
     }
+
+    /**
+     * Returns whether this dominion is eligible to tick.
+     *
+     * @param Dominion $dominion
+     * @return bool
+     */
+    public function canTick(Dominion $dominion)
+    {
+        return $this->isUnderProtection($dominion);
+    }
+
+    /**
+     * Returns whether this dominion is eligible to delete.
+     *
+     * @param Dominion $dominion
+     * @return bool
+     */
+    public function canDelete(Dominion $dominion): bool
+    {
+        $canDelete = false;
+
+        if(!$dominion->round->hasStarted() or $this->canTick($dominion))
+        {
+            $canDelete = true;
+        }
+
+        return $canDelete;
+    }
+
+
 }
