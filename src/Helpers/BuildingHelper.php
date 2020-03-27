604b85d01307838fd5a372506592db6dc123756c
Dr. Eki <dreki@lime.st>
2020-03-27 09:28:41 +0200

    Update BuildingHelper.php

--- src/Helpers/BuildingHelper.php
+++ src/Helpers/BuildingHelper.php
@@ -405,7 +405,7 @@ class BuildingHelper
       $buildings = [];
       foreach($allBuildings as $building)
       {
-        if(count(array_diff($building->excluded_races, [$race->name])) !== 0 and count(array_diff($building->exclusive_races, [$race->name])) == 0)
+        if(count(array_diff($building->excluded_races, [$race->name])) !== 0 or count(array_diff($building->exclusive_races, [$race->name])) == 1)
         {
           $buildings[] = $building;
         }
