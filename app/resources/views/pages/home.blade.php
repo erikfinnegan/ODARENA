c0b8d658295bd351e8245d34c8ac3b45defbbb8a
Dr. Eki <dreki@lime.st>
2020-04-01 09:50:28 +0300

    Update home.blade.php

--- app/resources/views/pages/home.blade.php
+++ app/resources/views/pages/home.blade.php
@@ -150,28 +150,48 @@
                     <h3 class="box-title">Welcome to OD Arena!</h3>
                 </div>
                 <div class="box-body">
-                    <p>OD Arena is a persistent browser-based fantasy game where you control a dominion and is charged with defending its lands and competing with other players to become the largest in the current round.</p>
-
-                    <p>To start playing, first <a href="{{ route('auth.register') }}">Register An Account</a>.</p>
-                    <p>If you already have an account, <a href="{{ route('auth.login') }}">Login To Your Account</a>.</p>
-                    <p>Then once you are logged in, you can create your Dominion and join the round.</p>
-
-                    @if ($currentRound === null || $currentRound->hasEnded())
-                    <p><em>There is currently no round. A new one will start in a day or two.</em></p>
-
-                      @if ($discordInviteLink = config('app.discord_invite_link'))
-                        <p>Join us on our <a href="{{ $discordInviteLink }}" target="_blank">Discord server <i class="fa fa-external-link"></i></a> to be informed.
-                      @endif
-
+                    @if(request()->getHost() == 'sim.odarena.com')
+                        <p>This is the <strong>ODARENA Simulator</strong>.</p>
+                        <p>The simulator is identical to the game. Here's how it works:</p>
+                        <ol>
+                          <li>Create a new account. The sim uses an entirely separate database from the game, in order to make changes here (for upcoming rounds) without affecting the game.</li>
+                          <li>Create a dominion as usual.</li>
+                          <li>Start ticking through protection like you would normally.</li>
+                          <li>Once you have depleted your protection ticks, you can delete and start a new dominion.</li>
+                          <li>Refresh the page and you will be able to create a new dominion.</li>
+                          <li>No scheduled ticks take place here &mdash; ever.</li>
+                        </ol>
+                        <p>Known bugs/issues:</p>
+                        <ul>
+                          <li>Deleting will generate an ugly Server 500 error for now. Just refresh and it goes away.</li>
+                          <li>OP calculation on invasion page isn’t working. Calculate OP manually for now.</li>
+                        </ul>
+                        <p>To start simming, first <a href="{{ route('auth.register') }}">Register An Account</a>.</p>
+                        <p>If you already have an account, <a href="{{ route('auth.login') }}">Login To Your Account</a>.</p>
                     @else
+                        <p>OD Arena is a persistent browser-based fantasy game where you control a dominion and is charged with defending its lands and competing with other players to become the largest in the current round.</p>
 
-                      @if ($discordInviteLink = config('app.discord_invite_link'))
-                        <p>And please come join us on our <a href="{{ $discordInviteLink }}" target="_blank">Discord server <i class="fa fa-external-link"></i></a>!
-                      @endif
+                        <p>To start playing, first <a href="{{ route('auth.register') }}">Register An Account</a>.</p>
+                        <p>If you already have an account, <a href="{{ route('auth.login') }}">Login To Your Account</a>.</p>
+                        <p>Then once you are logged in, you can create your Dominion and join the round.</p>
 
+                        @if ($currentRound === null || $currentRound->hasEnded())
+                            <p><em>There is currently no round. A new one will start in a day or two.</em></p>
+
+                                @if ($discordInviteLink = config('app.discord_invite_link'))
+                                  <p>Join us on our <a href="{{ $discordInviteLink }}" target="_blank">Discord server <i class="fa fa-external-link"></i></a> to be informed.
+                                @endif
+
+                        @else
+
+                          @if ($discordInviteLink = config('app.discord_invite_link'))
+                            <p>And please come join us on our <a href="{{ $discordInviteLink }}" target="_blank">Discord server <i class="fa fa-external-link"></i></a>!
+                          @endif
+
+                        @endif
+                         It's the main place for game announcements, game-related chat and development chat.</p>
+                       </p>
                     @endif
-                     It's the main place for game announcements, game-related chat and development chat.</p>
-                   </p>
                 </div>
                 <div class="box-body">
                     <p>OD Arena is based on <a href="https://beta.opendominion.net/" target="_new">OpenDominion</a>, created by WaveHack.</p>
