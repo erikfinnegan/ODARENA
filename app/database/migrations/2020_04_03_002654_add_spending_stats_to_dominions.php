89fd1db8737d665e788c996e48de334836628c0d
Dr. Eki <dreki@lime.st>
2020-04-03 18:52:56 +0300

    Update 2020_04_03_002654_add_spending_stats_to_dominions.php

--- app/database/migrations/2020_04_03_002654_add_spending_stats_to_dominions.php
+++ app/database/migrations/2020_04_03_002654_add_spending_stats_to_dominions.php
@@ -73,7 +73,7 @@ class AddSpendingStatsToDominions extends Migration
             $table->unsignedInteger('stat_total_wild_yeti_spent_training')->after('stat_total_soul_spent_training')->default(0);
 
             # Champion
-            $table->unsignedInteger('stat_total_champion_spent_training')->after('stat_total_wild_yeti_production_spent_training')->default(0);
+            $table->unsignedInteger('stat_total_champion_spent_training')->after('stat_total_wild_yeti_spent_training')->default(0);
 
         });
     }
