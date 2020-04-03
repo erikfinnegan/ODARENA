b085656a50748e516aff0b1dfa3523fb852214c4
Dr. Eki <dreki@lime.st>
2020-04-03 10:14:42 +0300

    Update production.blade.php

--- app/resources/views/pages/dominion/advisors/production.blade.php
+++ app/resources/views/pages/dominion/advisors/production.blade.php
@@ -336,50 +336,17 @@
                                   @php($jobsNeeded = ($selectedDominion->peasants - $populationCalculator->getEmploymentJobs($selectedDominion)))
                                   @if ($jobsNeeded < 0)
                                   <tr>
-                                    <td><span data-toggle="tooltip" data-placement="top" title="How many peasants you need in order to fill all available jobs">Jobs available:</span></td>
+                                    <td><span data-toggle="tooltip" data-placement="top" title="How many peasants you need in order to fill all available jobs.">Jobs available:</span></td>
                                     <td>{{ number_format(abs($jobsNeeded)) }}</td>
                                   </tr>
                                   @else
                                   <tr>
-                                    <td><span data-toggle="tooltip" data-placement="top" title="How many new jobs need to be created to provide employment for all currently unemployed peasants<br>Peasants - Jobs = Jobs Needed">Jobs needed:</span></td>
+                                    <td><span data-toggle="tooltip" data-placement="top" title="How many new jobs need to be created to provide employment for all currently unemployed peasants.<br>Peasants - Jobs = Jobs Needed">Jobs needed:</span></td>
                                     <td>{{ number_format(abs($jobsNeeded)) }}</td>
                                   </tr>
                                   @endif
                                 </tbody>
                               </table>
-
-                              <br>
-
-
-                              Total: {{ number_format($populationCalculator->getPopulation($selectedDominion)) }} / {{ number_format($populationCalculator->getMaxPopulation($selectedDominion)) }}<br>
-                              Peasants: {{ number_format($selectedDominion->peasants) }} / {{ number_format($populationCalculator->getMaxPopulation($selectedDominion) - $populationCalculator->getPopulationMilitary($selectedDominion)) }}
-                              @if ($selectedDominion->peasants_last_hour < 0)
-                                  <span class="text-red">(<b>{{ number_format($selectedDominion->peasants_last_hour) }}</b> last tick)</span>
-                              @elseif ($selectedDominion->peasants_last_hour > 0)
-                                  <span class="text-green">(<b>+{{ number_format($selectedDominion->peasants_last_hour) }}</b> last tick)</span>
-                              @endif
-                              <br>
-                              Military: {{ number_format($populationCalculator->getPopulationMilitary($selectedDominion)) }}<br>
-                              <br>
-                              <b>Jobs</b><br>
-                              Fulfilled: {{ number_format($populationCalculator->getPopulationEmployed($selectedDominion)) }} / {{ number_format($populationCalculator->getEmploymentJobs($selectedDominion)) }}<br>
-                              @php($jobsNeeded = ($selectedDominion->peasants - $populationCalculator->getEmploymentJobs($selectedDominion)))
-                              @if ($jobsNeeded < 0)
-                                  Available: {{ number_format(abs($jobsNeeded)) }}<br>
-                                  Opportunity cost of job overrun: <b>{{ number_format(2.7 * abs($jobsNeeded) * $productionCalculator->getPlatinumProductionMultiplier($selectedDominion)) }} platinum</b><br>
-                                  <br>
-                                  <i>"You should acquire additional peasants, since you have idle jobs.<br><br>Employed peasants pay their income tax in platinum to the dominion." -Production Advisor</i>
-                              @elseif ($jobsNeeded === 0)
-                                  Available: 0<br>
-                                  No opportunity cost
-                              @else
-                                  Needed: {{ number_format($jobsNeeded) }}<br>
-                                  Opportunity cost of job underrun: <b>{{ number_format(2.7 * $jobsNeeded * $productionCalculator->getPlatinumProductionMultiplier($selectedDominion)) }} platinum</b><br>
-                                  <br>
-                                  <i>"You should construct additional job buildings, since you have idle peasants.<br><br>Only employed peasants pay their income tax in platinum to the dominion." -Production Advisor</i>
-                              @endif
-                            @endif
-                        </p>
                     </div>
                 </div>
             </div>
