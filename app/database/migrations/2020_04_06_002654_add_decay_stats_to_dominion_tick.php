ba7d342cbccc4b40a39e8fc0e3369e1cd2d80b49
Dr. Eki <dreki@lime.st>
2020-04-06 14:51:10 +0300

    Update 2020_04_06_002654_add_decay_stats_to_dominion_tick.php

--- app/database/migrations/2020_04_06_002654_add_decay_stats_to_dominion_tick.php
+++ app/database/migrations/2020_04_06_002654_add_decay_stats_to_dominion_tick.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDecayStatsToDominionTicks extends Migration
+class AddDecayStatsToDominionTick extends Migration
 {
     /**
      * Run the migrations.
