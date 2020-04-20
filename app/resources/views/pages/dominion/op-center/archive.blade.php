e8466c9f36985e3f415b45ad5951d69d7f42b161
Dr. Eki <dreki@lime.st>
2020-04-20 13:40:59 +0300

    Update archive.blade.php

--- app/resources/views/pages/dominion/op-center/archive.blade.php
+++ app/resources/views/pages/dominion/op-center/archive.blade.php
@@ -31,7 +31,7 @@
                     <h3 class="box-title">Information</h3>
                 </div>
                 <div class="box-body">
-                <p>Sections marked as <span class="label label-warning">stale</span> contain data from the previous hour (or earlier) and should be considered inaccurate. Sections marked as <span class="label label-danger">invalid</span> are more than 12 hours old. Recast your info ops before performing any offensive operations during this hour.</p>
+                @include('partials.dominion.op-center.labels-explainer')
                 </div>
             </div>
         </div>
@@ -151,11 +151,19 @@
                                             <td>Champions:</td>
                                             <td>{{ number_format($infoOp->data['resource_champion']) }}</td>
                                         </tr>
-                                        @elseif ($dominion->race->name == 'Demon' and isset($infoOp->data['resource_soul']))
-                                        <tr>
-                                            <td>Souls:</td>
-                                            <td>{{ number_format($infoOp->data['resource_soul']) }}</td>
-                                        </tr>
+                                        @elseif ($dominion->race->name == 'Demon')
+                                            @if(isset($infoOp->data['resource_soul']))
+                                            <tr>
+                                                <td>Souls:</td>
+                                                <td>{{ number_format($infoOp->data['resource_soul']) }}</td>
+                                            </tr>
+                                            @endif
+                                            @if(isset($infoOp->data['resource_blood']))
+                                            <tr>
+                                                <td>Blood:</td>
+                                                <td>{{ number_format($infoOp->data['resource_blood']) }}</td>
+                                            </tr>
+                                            @endif
                                         @elseif ($dominion->race->name == 'Snow Elf' and isset($infoOp->data['resource_wild_yeti']))
                                         <tr>
                                             <td>Wild yetis:</td>
@@ -345,7 +353,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($improvementHelper->getImprovementTypes($dominion->race->name) as $improvementType)
+                                @foreach ($improvementHelper->getImprovementTypes($dominion) as $improvementType)
                                     <tr>
                                         <td>
                                           <i class="ra ra-{{ $improvementHelper->getImprovementIcon($improvementType) }} ra-fw" data-toggle="tooltip" data-placement="top" title="{{ $improvementHelper->getImprovementHelpString($improvementType, $selectedDominion) }}"></i>
