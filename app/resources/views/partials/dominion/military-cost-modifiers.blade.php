4e3226dc57a9ba98cfd927a6b219afc85370fceb
Dr. Eki <dreki@lime.st>
2020-04-20 14:05:02 +0300

    R21

---
+++ app/resources/views/partials/dominion/military-cost-modifiers.blade.php
@@ -0,0 +1,42 @@
+<div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title"><i class="ra ra-anvil"></i> Military Cost Modifiers</h3>
+    </div>
+        <div class="box-body table-responsive no-padding">
+            <table class="table">
+                <colgroup>
+                    <col width="33%">
+                    <col width="33%">
+                    <col width="33%">
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th>Resource</th>
+                        <th>Modifier</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Platinum:</td>
+                        <td>{{ number_format(($trainingCalculator->getSpecialistEliteCostMultiplier($selectedDominion, 'platinum')-1) * 100, 2) }}%</td>
+                    </tr>
+                    <tr>
+                        <td>Ore:</td>
+                        <td>{{ number_format(($trainingCalculator->getSpecialistEliteCostMultiplier($selectedDominion, 'ore')-1) * 100, 2) }}%</td>
+                    </tr>
+                    <tr>
+                        <td>Lumber:</td>
+                        <td>{{ number_format(($trainingCalculator->getSpecialistEliteCostMultiplier($selectedDominion, 'lumber')-1) * 100, 2) }}%</td>
+                    </tr>
+                    <tr>
+                        <td>Mana:</td>
+                        <td>{{ number_format(($trainingCalculator->getSpecialistEliteCostMultiplier($selectedDominion, 'mana')-1) * 100, 2) }}%</td>
+                    </tr>
+                    <tr>
+                        <td>Food:</td>
+                        <td>{{ number_format(($trainingCalculator->getSpecialistEliteCostMultiplier($selectedDominion, 'food')-1) * 100, 2) }}%</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+</div>
