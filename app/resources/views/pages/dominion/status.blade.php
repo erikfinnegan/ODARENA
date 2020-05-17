649fe35908ab387dc3caed45c462ba3c51f0b265
Dr. Eki <dreki@lime.st>
2020-05-17 20:12:57 +0300

    Update status.blade.php

--- app/resources/views/pages/dominion/status.blade.php
+++ app/resources/views/pages/dominion/status.blade.php
@@ -27,7 +27,7 @@
                                 <tbody>
                                     <tr>
                                         <td>Ruler:</td>
-                                        <td><em>{{ $selectedDominion->title->name }}</em> {{ $selectedDominion->ruler_name }}</td>
+                                        <td>@if(isset($selectedDominion->title->name))<em>{{ $selectedDominion->title->name }}</em>@endif {{ $selectedDominion->ruler_name }}</td>
                                     </tr>
                                     <tr>
                                         <td>Faction:</td>
