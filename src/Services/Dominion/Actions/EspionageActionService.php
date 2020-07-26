e5214800702d5d8320f29027be8a46bf56703e66
Dr. Eki <dreki@lime.st>
2020-07-26 06:28:36 +0300

    R27

--- src/Services/Dominion/Actions/EspionageActionService.php
+++ src/Services/Dominion/Actions/EspionageActionService.php
@@ -392,7 +392,7 @@ class EspionageActionService
                 $data = [
                     'units' => [
                         'home' => [],
-                        'returning' => [],
+                        'invasion' => [],
                         'training' => [],
                     ],
                 ];
