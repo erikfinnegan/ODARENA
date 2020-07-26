891e7fd0938224cec94967b5e5ef73cff3991532
Dr. Eki <dreki@lime.st>
2020-07-26 11:46:36 +0300

    Update TickService.php

--- src/Services/Dominion/TickService.php
+++ src/Services/Dominion/TickService.php
@@ -487,7 +487,7 @@ class TickService
         {
           // Are we invading?
           $invade = false;
-          
+
           // Make sure all units1 and unit4 are at home.
           if($dominion->military_unit1 > 0 and
              $dominion->military_unit4 > 0 and
@@ -502,6 +502,8 @@ class TickService
                   $invade = true;
               }
           }
+
+          $invade = false;
 
             if($invade)
             {
