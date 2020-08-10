6667f53ee20d44634c83a48df7d1eadda94b0a66
Dr. Eki <dreki@lime.st>
2020-08-10 15:23:06 +0300

    Update main-footer.blade.php

--- app/resources/views/partials/main-footer.blade.php
+++ app/resources/views/partials/main-footer.blade.php
@@ -2,17 +2,21 @@
 
     <div class="pull-right">
 
-      @if (isset($selectedDominion) && ($selectedDominion->round->isActive()))
+      @if (isset($selectedDominion) and !$selectedDominion->round->hasEnded())
           @php
           $diff = $selectedDominion->round->start_date->subDays(1)->diff(now());
           $roundDay = $selectedDominion->round->start_date->subDays(1)->diffInDays(now());
           $roundDurationInDays = $selectedDominion->round->durationInDays();
           $currentHour = ($diff->h + 1);
           $currentTick = 1+floor(intval(Date('i')) / 15);
-
-          echo "Day <strong>{$roundDay}</strong>/{$roundDurationInDays}, hour <strong>{$currentHour}</strong>, tick <strong>{$currentTick}</strong>.";
-
           @endphp
+
+          Day <strong>{{ $roundDay }}</strong>/{{ $roundDurationInDays }}, hour <strong>{{ $currentHour }}</strong>, tick <strong>{{ $currentTick }}</strong>.
+
+      @elseif (isset($selectedDominion) and !$selectedDominion->round->hasStarted())
+
+          Round <strong>{{ $selectedDominion->round->number }}</strong> starts in {{ now()->startOfHour()->diffInHours(Carbon::parse($selectedDominion->round->start_date)->startOfHour()); }} hours.
+
       @endif
       <br>
 
