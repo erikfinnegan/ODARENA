89f5b493603141739a135b9ca049c6dea76a2ac4
Dr. Eki <dreki@lime.st>
2020-09-07 06:47:39 +0300

    R30

--- src/Services/Dominion/BarbarianService.php
+++ src/Services/Dominion/BarbarianService.php
@@ -23,7 +23,7 @@ class BarbarianService
 {
 
     protected const DPA_CONSTANT = 25;
-    protected const DPA_PER_HOUR = 0.50;
+    protected const DPA_PER_HOUR = 0.40;
 
     # Train % of new units as specs. /1000
     protected const SPECS_RATIO_MIN = 50;
@@ -33,8 +33,8 @@ class BarbarianService
     protected const ONE_IN_CHANCE_TO_HIT = 2;
 
     # Gain % of land between these two values when hitting. /1000
-    protected const LAND_GAIN_MIN = 100;
-    protected const LAND_GAIN_MAX = 200;
+    protected const LAND_GAIN_MIN = 80;
+    protected const LAND_GAIN_MAX = 120;
 
     # Send between these two values when hitting. /100
     protected const SENT_RATIO_MIN = 80;
