ec31262027387337d4c8ef0583a7e00576f5cac9
Dr. Eki <dreki@lime.st>
2020-12-24 06:30:08 +0200

    R37

--- app/resources/views/partials/dominion/spell-basics.blade.php
+++ app/resources/views/partials/dominion/spell-basics.blade.php
@@ -4,14 +4,20 @@
 
 <span data-toggle="tooltip" data-placement="top" title="Mana required to cast spell">M</span>: <span class="{{ $manaAfforded }}">{{ number_format($spellCalculator->getManaCost($selectedDominion, $spell->key)) }}</span>
 @if($spell->duration > 0)
-  / <span data-toggle="tooltip" data-placement="top" title="Tick duration of effect">{{--T--}}<i class="ra ra-hourglass"></i></span>: {{ $spell->duration }}
+  / <span data-toggle="tooltip" data-placement="top" title="Tick duration of effect">{{--T--}}<i class="ra ra-hourglass"></i></span>:
+        @if($isActive)
+            <span class="text-green">{{ $spellCalculator->getSpellDuration($selectedDominion, $spell->key) }}/{{ $spell->duration }}</span>
+        @else
+            <span class="text-muted">{{ $spell->duration }}</span>
+        @endif
+         ticks
 @endif
 @if($spell->cooldown > 0)
   / <span data-toggle="tooltip" data-placement="top" title="Cooldown until spell can be cast again">CD</span>:
         @if($spellCalculator->isOnCooldown($selectedDominion, $spell))
             <span class="text-red">{{ $spellCalculator->getSpellCooldown($selectedDominion, $spell) }}/{{ $spell->cooldown }}h</span>
         @else
-            {{ $spell->cooldown }}h
+            <span class="text-muted">{{ $spell->cooldown }}h</span>
         @endif
 @endif
 @if($spell->wizard_strength)
