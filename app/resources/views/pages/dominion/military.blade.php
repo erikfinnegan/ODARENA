2d4391ef4beaecf3f12465f229ff03316e49ce60
Dr. Eki <dreki@lime.st>
2020-12-26 07:53:29 +0200

    R37

--- app/resources/views/pages/dominion/military.blade.php
+++ app/resources/views/pages/dominion/military.blade.php
@@ -237,113 +237,111 @@
             </form>
         </div>
 
-
-            <div class="col-sm-12 col-md-6">
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><i class="ra ra-sword"></i> Units in training and home</h3>
-                    </div>
-                    <div class="box-body table-responsive no-padding">
-                        <table class="table">
-                            <colgroup>
-                                <col>
+        <div class="col-sm-12 col-md-6">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="ra ra-sword"></i> Units in training and home</h3>
+                </div>
+                <div class="box-body table-responsive no-padding">
+                    <table class="table">
+                        <colgroup>
+                            <col>
+                            @for ($i = 1; $i <= 12; $i++)
+                                <col width="20">
+                            @endfor
+                            <col width="100">
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>Unit</th>
                                 @for ($i = 1; $i <= 12; $i++)
-                                    <col width="20">
+                                    <th class="text-center">{{ $i }}</th>
                                 @endfor
-                                <col width="100">
-                            </colgroup>
-                            <thead>
+                                <th class="text-center">Home (Training)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($unitHelper->getUnitTypes() as $unitType)
                                 <tr>
-                                    <th>Unit</th>
-                                    @for ($i = 1; $i <= 12; $i++)
-                                        <th class="text-center">{{ $i }}</th>
-                                    @endfor
-                                    <th class="text-center">Home (Training)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($unitHelper->getUnitTypes() as $unitType)
-                                    <tr>
-                                        <td>
+                                    <td>
 
-                                            <span data-toggle="tooltip" data-placement="top" title="{{ $unitHelper->getUnitHelpString($unitType, $selectedDominion->race) }}">
-                                                {{ $unitHelper->getUnitName($unitType, $selectedDominion->race) }}
-                                            </span>
-                                        </td>
-                                        @for ($i = 1; $i <= 12; $i++)
-                                            <td class="text-center">
-                                                @if ($queueService->getTrainingQueueAmount($selectedDominion, "military_{$unitType}", $i) === 0)
-                                                    -
-                                                @else
-                                                    {{ number_format($queueService->getTrainingQueueAmount($selectedDominion, "military_{$unitType}", $i)) }}
-                                                @endif
-                                            </td>
-                                        @endfor
+                                        <span data-toggle="tooltip" data-placement="top" title="{{ $unitHelper->getUnitHelpString($unitType, $selectedDominion->race) }}">
+                                            {{ $unitHelper->getUnitName($unitType, $selectedDominion->race) }}
+                                        </span>
+                                    </td>
+                                    @for ($i = 1; $i <= 12; $i++)
                                         <td class="text-center">
-                                            {{ number_format($selectedDominion->{'military_' . $unitType}) }}
-                                            ({{ number_format($queueService->getTrainingQueueTotalByResource($selectedDominion, "military_{$unitType}")) }})
+                                            @if ($queueService->getTrainingQueueAmount($selectedDominion, "military_{$unitType}", $i) === 0)
+                                                -
+                                            @else
+                                                {{ number_format($queueService->getTrainingQueueAmount($selectedDominion, "military_{$unitType}", $i)) }}
+                                            @endif
                                         </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                                    @endfor
+                                    <td class="text-center">
+                                        {{ number_format($selectedDominion->{'military_' . $unitType}) }}
+                                        ({{ number_format($queueService->getTrainingQueueTotalByResource($selectedDominion, "military_{$unitType}")) }})
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
+        </div>
 
-            <div class="col-sm-12 col-md-6">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><i class="fa fa-clock-o"></i> Units returning from battle</h3>
-                    </div>
-                    <div class="box-body table-responsive no-padding">
-                        <table class="table">
-                            <colgroup>
-                                <col>
+        <div class="col-sm-12 col-md-6">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-clock-o"></i> Units returning from battle</h3>
+                </div>
+                <div class="box-body table-responsive no-padding">
+                    <table class="table">
+                        <colgroup>
+                            <col>
+                            @for ($i = 1; $i <= 12; $i++)
+                                <col width="20">
+                            @endfor
+                            <col width="100">
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>Unit</th>
                                 @for ($i = 1; $i <= 12; $i++)
-                                    <col width="20">
+                                    <th class="text-center">{{ $i }}</th>
                                 @endfor
-                                <col width="100">
-                            </colgroup>
-                            <thead>
+                                <th class="text-center">Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach (range(1, 4) as $slot)
+                                @php($unitType = ('unit' . $slot))
                                 <tr>
-                                    <th>Unit</th>
-                                    @for ($i = 1; $i <= 12; $i++)
-                                        <th class="text-center">{{ $i }}</th>
-                                    @endfor
-                                    <th class="text-center">Total</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach (range(1, 4) as $slot)
-                                    @php($unitType = ('unit' . $slot))
-                                    <tr>
-                                        <td>
+                                    <td>
 
-                                            <span data-toggle="tooltip" data-placement="top" title="{{ $unitHelper->getUnitHelpString($unitType, $selectedDominion->race) }}">
-                                                {{ $unitHelper->getUnitName($unitType, $selectedDominion->race) }}
-                                            </span>
-                                        </td>
-                                        @for ($i = 1; $i <= 12; $i++)
-                                            <td class="text-center">
-                                                @if ($queueService->getInvasionQueueAmount($selectedDominion, "military_{$unitType}", $i) === 0)
-                                                    -
-                                                @else
-                                                    {{ number_format($queueService->getInvasionQueueAmount($selectedDominion, "military_{$unitType}", $i)) }}
-                                                @endif
-                                            </td>
-                                        @endfor
+                                        <span data-toggle="tooltip" data-placement="top" title="{{ $unitHelper->getUnitHelpString($unitType, $selectedDominion->race) }}">
+                                            {{ $unitHelper->getUnitName($unitType, $selectedDominion->race) }}
+                                        </span>
+                                    </td>
+                                    @for ($i = 1; $i <= 12; $i++)
                                         <td class="text-center">
-                                            {{ number_format($queueService->getInvasionQueueTotalByResource($selectedDominion, "military_{$unitType}")) }}
+                                            @if ($queueService->getInvasionQueueAmount($selectedDominion, "military_{$unitType}", $i) === 0)
+                                                -
+                                            @else
+                                                {{ number_format($queueService->getInvasionQueueAmount($selectedDominion, "military_{$unitType}", $i)) }}
+                                            @endif
                                         </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                                    @endfor
+                                    <td class="text-center">
+                                        {{ number_format($queueService->getInvasionQueueTotalByResource($selectedDominion, "military_{$unitType}")) }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
-
+        </div>
 
     </div>
 
@@ -352,7 +350,7 @@
         <div class="box">
             <div class="box-header with-border">
                 <h3 class="box-title">Drafting</h3>
-                <a href="{{ route('dominion.advisors.military') }}" class="pull-right">Release Units</a>
+                <a href="{{ route('dominion.military.release') }}" class="pull-right">Release Units</a>
             </div>
             <form action="{{ route('dominion.military.change-draft-rate') }}" method="post" role="form">
                 @csrf
@@ -398,11 +396,4 @@
     </div>
 
 </div>
-
-<div class="row">
-
-</div>
-
-
-
 @endsection
