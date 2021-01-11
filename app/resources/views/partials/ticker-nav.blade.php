a3ebc8ba3bfd5ec7eb6939c4b4695dfed6d1b309
Dr. Eki <dreki@lime.st>
2021-01-11 10:22:50 +0200

    R38

--- app/resources/views/partials/ticker-nav.blade.php
+++ app/resources/views/partials/ticker-nav.blade.php
@@ -1,3 +1,3 @@
 <li>
-    <a href="{{ route('dominion.status') }}"><i class="far fa-clock"></i><span id="ticker-server">{{ date('H:i:s') }}</span></a>
+    <a href="{{ route('dominion.status') }}"><i class="far fa-clock fa-fw"></i><span id="ticker-server">{{ date('H:i:s') }}</span></a>
 </li>
