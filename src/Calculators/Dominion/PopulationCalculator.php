148b3bc1b34b037d5c5db1267ac3764e6e70dff2
Dr. Eki <dreki@lime.st>
2021-02-20 06:41:32 +0200

    R40

--- src/Calculators/Dominion/PopulationCalculator.php
+++ src/Calculators/Dominion/PopulationCalculator.php
@@ -217,7 +217,7 @@ class PopulationCalculator
     */
     public function getAvailableHousingFromForestHavens(Dominion $dominion): int
     {
-        return $dominion->getBuildingProduction('spy_housing') * (1 + $this->improvementCalculator->getImprovementMultiplierBonus($dominion, 'hideouts'));
+        return $dominion->getBuildingPerkValue('spy_housing') * (1 + $this->improvementCalculator->getImprovementMultiplierBonus($dominion, 'hideouts'));
     }
 
     /*
@@ -225,7 +225,7 @@ class PopulationCalculator
     */
     public function getAvailableHousingFromWizardGuilds(Dominion $dominion): int
     {
-        return $dominion->getBuildingProduction('wizard_housing') * (1 + $this->improvementCalculator->getImprovementMultiplierBonus($dominion, 'spires'));
+        return $dominion->getBuildingPerkValue('wizard_housing') * (1 + $this->improvementCalculator->getImprovementMultiplierBonus($dominion, 'spires'));
     }
 
     /*
