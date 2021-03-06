c48b55acda07998efd2fba568eb8d1d96b3490d0
Dr. Eki <dreki@lime.st>
2021-03-06 06:43:37 +0200

    R42

--- src/Helpers/RaceHelper.php
+++ src/Helpers/RaceHelper.php
@@ -266,6 +266,11 @@ class RaceHelper
                 $description = 'Military housing in buildings that provide military housing';
                 $valueType = '%';
                 break;
+            case 'amount_stolen':
+                $negativeBenefit = false;
+                $description = 'Amount stolen';
+                $valueType = '%';
+                break;            
             case 'cannot_build_homes':
                 $negativeBenefit = true;
                 $description = 'Cannot build Homes';
