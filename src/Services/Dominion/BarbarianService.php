bd07e9ab61a2d3f47ffdf50115403c707f56b88e
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2021-04-12 21:32:28 +0300

    R45

--- src/Services/Dominion/BarbarianService.php
+++ src/Services/Dominion/BarbarianService.php
@@ -450,7 +450,7 @@ class BarbarianService
                 }
 
                 # Chicken out: 11/12 chance that the Barbarians won't hit.
-                if(rand(1, 8) !== 1)
+                if($invadePlayer and rand(1, 8) !== 1)
                 {
                     $logString .= "\t\t** " . $dominion->name . ' chickens out from invading ' . $target->name . "! 🐤\n";
                     $invadePlayer = false;
