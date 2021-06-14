168b03930e0720ce78235181d2f65e0e5488ac45
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2021-06-14 21:42:11 +0300

    R50

--- src/Models/Dominion.php
+++ src/Models/Dominion.php
@@ -979,7 +979,7 @@ class Dominion extends AbstractModel
         $multiplier += $this->getBuildingPerkMultiplier('improvements');
         $multiplier += $this->getSpellPerkMultiplier('improvements');
         $multiplier += $this->getTechPerkMultiplier('improvements');
-        $multiplier += $this->race->getPerkMultiplier('max_improvements');
+        $multiplier += $this->race->getPerkMultiplier('improvements_max');
         #if($this->title)
         #{
         #    $multiplier += $this->title->getPerkMultiplier('improvements') * $this->title->getPerkBonus($this);
