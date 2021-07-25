e24804821d90948a31f1f0bf91fd0f9478320c09
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2021-07-25 16:11:10 +0300

    R54

--- src/Helpers/BuildingHelper.php
+++ src/Helpers/BuildingHelper.php
@@ -99,8 +99,8 @@ class BuildingHelper
             'extra_units_trained' => '%2$s%% additional units trained for free for every %1$s%% (max %3$s%% extra units).',
 
             'faster_return' =>  '%2$s%% of units sent on invasion return %4$s ticks faster for every %1$s%% (max %3$s%% of all units).',
-            'faster_return_units_increasing' =>  '%2$s%% of units sent on invasion return %4$s ticks faster for every %1$s%% (max %3$s%% of all units).',
-            'faster_return_units' =>  '%2$s%% of units sent on invasion return %4$s ticks faster for every %1$s%% (max %3$s%% of all units).',
+            'faster_returning_units_increasing' =>  'Each building enables %1$s units sent on invasion to return four ticks faster, increased by %2$s per tick.',
+            'faster_returning_units' =>  'Each building enables %1$s units sent on invasion to return four ticks faster.',
 
             'morale_gains' => 'Morale gains increased by %2$s%% for every %1$s%% (max +%3$s%%)',
             'base_morale' => 'Base morale increased by %2$s%% for every %1$s%% (max +%3$s%%)',
