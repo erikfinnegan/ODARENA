6705e5eebbce354b22bd5960fee1a85b7f086a59
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2021-08-19 21:06:39 +0300

    R55

--- app/resources/views/pages/dominion/world-news.blade.php
+++ app/resources/views/pages/dominion/world-news.blade.php
@@ -52,7 +52,7 @@
                                                     @if ($gameEvent->data['result']['success'])
                                                         @if(isset($gameEvent->data['attacker']['liberation']) and $gameEvent->data['attacker']['liberation'])
                                                             Victorious on the battlefield,
-                                                            <span class="text-aqua">{{ $gameEvent->source->name }} <a href="{{ route('dominion.realm', [$gameEvent->source->realm->number]) }}">(#{{ $gameEvent->source->realm->number }})</a></span>
+                                                            <span class="text-aqua"><a href="{{ route('dominion.insight.show', [$gameEvent->source->id]) }}">{{ $gameEvent->source->name }}</a> <a href="{{ route('dominion.realm', [$gameEvent->source->realm->number]) }}">(#{{ $gameEvent->source->realm->number }})</a></span>
                                                             liberated
                                                             <span data-toggle="tooltip" data-placement="top" title="{{ $gameEvent->target->race->name }} ({{ number_format($landCalculator->getTotalLand($gameEvent->target)/$landCalculator->getTotalLand($selectedDominion)*100,2) }}%)">
                                                             <a href="{{ route('dominion.insight.show', [$gameEvent->target->id]) }}"><span class="text-orange">{{ $gameEvent->target->name }}</span></a>
@@ -107,7 +107,7 @@
                                                         @if ($gameEvent->source_realm_id == $selectedDominion->realm_id)
                                                             Fellow dominion
                                                         @endif
-                                                        <span class="text-aqua">{{ $gameEvent->target->name }} <a href="{{ route('dominion.realm', [$gameEvent->target->realm->number]) }}">(#{{ $gameEvent->target->realm->number }})</a></span>
+                                                        <span class="text-aqua"><a href="{{ route('dominion.insight.show', [$gameEvent->target->id]) }}">{{ $gameEvent->target->name }}</a> <a href="{{ route('dominion.realm', [$gameEvent->target->realm->number]) }}">(#{{ $gameEvent->target->realm->number }})</a></span>
                                                         fended off an attack from
                                                         <span data-toggle="tooltip" data-placement="top" title="{{ $gameEvent->source->race->name }} ({{ number_format($landCalculator->getTotalLand($gameEvent->source)/$landCalculator->getTotalLand($selectedDominion)*100,2) }}%)">
                                                         <a href="{{ route('dominion.insight.show', [$gameEvent->source->id]) }}"><span class="text-orange">{{ $gameEvent->source->name }}</span></a>
