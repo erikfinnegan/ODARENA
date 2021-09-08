5bee67cea7b4e9077bf9dae6f3cfcb8f4d9240a7
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2021-09-08 09:15:21 +0300

    R56

--- src/Models/Race.php
+++ src/Models/Race.php
@@ -27,7 +27,6 @@ class Race extends AbstractModel
       protected $casts = [
           'resources' => 'array',
           'improvement_resources' => 'array',
-          'land_improvements' => 'array',
           'peasants_production' => 'array',
           'home_land_type' => 'text',
           'peasants_alias' => 'text',
