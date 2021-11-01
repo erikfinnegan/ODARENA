e1c0833a7c8a2be9f3aa108f38044c061598acdf
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2021-11-01 19:28:13 +0200

    R59

--- src/Services/GameEventService.php
+++ src/Services/GameEventService.php
@@ -71,7 +71,7 @@ class GameEventService
         $gameEvents = GameEvent::query()
             ->where('round_id', $dominion->round_id)
             ->where('created_at', '<', $createdBefore)
-            ->where('created_at', '>', now()->subDays(7))
+            ->where('created_at', '>', now()->subHours(48))
             ->orderBy('created_at', 'desc')
             ->get();
 
