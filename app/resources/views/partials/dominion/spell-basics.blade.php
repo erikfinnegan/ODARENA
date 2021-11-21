b1b944dae4e07cc1d9830e31dfd0316711df9fe7
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2021-11-21 07:46:54 +0200

    R61

--- app/resources/views/partials/dominion/spell-basics.blade.php
+++ app/resources/views/partials/dominion/spell-basics.blade.php
@@ -4,22 +4,26 @@
 
 <span class="{{ $manaAfforded }}"  data-toggle="tooltip" data-placement="top" title="Mana required to cast spell">{{ number_format($spellCalculator->getManaCost($selectedDominion, $spell->key)) }}</span>
 @if($spell->duration > 0)
-  / <span data-toggle="tooltip" data-placement="top" title="Duration (ticks)"><i class="fas fa-hourglass-start"></i></span>:
+  / <span data-toggle="tooltip" data-placement="top" title="Duration (ticks)">
+        <i class="fas fa-hourglass-start"></i>:
         @if(isset($isActive) and $isActive)
             <span class="text-green">{{ number_format($spellCalculator->getSpellDuration($selectedDominion, $spell->key)) }}/{{ number_format($spell->duration) }}</span>
         @else
             <span class="text-muted">{{ number_format($spell->duration) }}</span>
         @endif
-         ticks
+        ticks
+    </span>
 @endif
 @if($spell->cooldown > 0)
-  / <span data-toggle="tooltip" data-placement="top" title="Cooldown until spell can be cast again (ticks)"><i class="fas fa-hourglass-end"></i></i></span>:
+  / <span data-toggle="tooltip" data-placement="top" title="Cooldown until spell can be cast again (ticks)">
+        <i class="fas fa-hourglass-end"></i>:
         @if($spellCalculator->isOnCooldown($selectedDominion, $spell))
             <span class="text-red">{{ number_format($spellCalculator->getSpellCooldown($selectedDominion, $spell)) }}/{{ number_format($spell->cooldown) }}</span>
         @else
             <span class="text-muted">{{ number_format($spell->cooldown) }}</span>
         @endif
         ticks
+    </span>
 @endif
 @if($spell->wizard_strength)
   / <span data-toggle="tooltip" data-placement="top" title="Wizard stregth required to cast spell">WS</span>: {{ $spell->wizard_strength }}%
