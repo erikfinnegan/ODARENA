6189da3ee0ae5f0e84520eae2f3c11af41b16c03
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2021-11-27 19:53:32 +0200

    R61

--- app/resources/views/pages/dominion/search.blade.php
+++ app/resources/views/pages/dominion/search.blade.php
@@ -82,7 +82,6 @@
                             <col width="100">
                             <col width="100">
                             <col width="100">
-                            <col width="100">
                         </colgroup>
                         <thead>
                             <tr>
