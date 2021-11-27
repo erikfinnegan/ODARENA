75732b1c5fcff395854a91ef5411b21153a4b200
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2021-11-27 15:54:39 +0200

    R61

--- app/resources/views/pages/dashboard.blade.php
+++ app/resources/views/pages/dashboard.blade.php
@@ -3,32 +3,19 @@
 @section('page-header', 'Dashboard')
 
 @section('content')
-    <div class="box">
-        <div class="box-body">
-            @if ($dominions->isEmpty())
-                <p>Welcome to ODARENA!</p>
-                <p>To start playing, please register in a round below.</p>
-            @else
-                <p>Welcome back to ODARENA, {{ Auth::user()->display_name }}!</p>
-                <p>Select a dominion below to go to its status screen.</p>
-            @endif
-        </div>
-    </div>
-
     <div class="row">
-
-        <div class="col-lg-12">
+        <div class="col-lg-9">
             <div class="box box-primary">
                 <table class="table table-striped">
                     <colgroup>
                         <col width="60">
                         <col width="120">
                         <col width="180">
+                        <col width="360">
+                        <col width="180">
+                        <col width="120">
+                        <col width="120">
                         <col>
-                        <col width="120">
-                        <col width="120">
-                        <col width="120">
-                        <col width="120">
                     </colgroup>
                     <thead>
                         <tr>
@@ -60,8 +47,7 @@
                             <td>
                                 @if(isset($dominion))
                                     @if ($dominion->isSelectedByAuthUser())
-                                        <a href="{{ route('dominion.status') }}">{{ $dominion->name }}</a>
-                                        <span class="label label-success">Selected</span>
+                                        <a href="{{ route('dominion.status') }}">{{ $dominion->name }}</a>&nbsp;<span class="label label-success">Selected</span>
                                     @else
                                         <form action="{{ route('dominion.select', $dominion) }}" method="post">
                                             @csrf
@@ -76,7 +62,7 @@
                                 @if($roundService->hasUserDominionInRound($round))
 
                                     @if($round->hasEnded())
-                                        <span data-toggle="tooltip" data-placement="top" title="Start: {{ $round->start_date }}.<br>Registration: {{ $dominion->created_at }}.<br>Ended: {{ $round->end_date }}">
+                                        <span data-toggle="tooltip" data-placement="top" title="Start: {{ $round->start_date }}.<br>Registration: {{ $dominion->created_at }}.<br>Ended: {{ $round->end_date }}.">
                                             <span class="label label-info">Finished</span>
                                         </span>
                                     @endif
@@ -88,24 +74,31 @@
                                     @endif
 
                                     @if(!$round->hasEnded() and !$dominion->isLocked())
-                                        <span data-toggle="tooltip" data-placement="top" title="Start: {{ $round->start_date }}.<br>Registration: {{ $dominion->created_at }}.">
+                                        <span data-toggle="tooltip" data-placement="top" title="Start: {{ $round->start_date }}.<br>Current tick: {{ number_format($round->ticks) }}.<br>You joined: {{ $dominion->created_at }}.">
                                             <span class="label label-success">Playing</span>
                                         </span>
                                     @endif
 
                                 @else
                                     @if($round->hasEnded() and $user->created_at <= $round->start_date)
-                                        <span data-toggle="tooltip" data-placement="top" title="Start: {{ $round->start_date }}.<br>Ended: {{ $round->end_date }}">
+                                        <span data-toggle="tooltip" data-placement="top" title="Start: {{ $round->start_date }}.<br>Ended: {{ $round->end_date }}.">
                                             <span class="label label-primary">Ended</span>
                                         </span>
                                     @elseif($round->hasEnded() and $user->created_at > $round->start_date)
-                                        <span data-toggle="tooltip" data-placement="top" title="Start: {{ $round->start_date }}.<br>Ended: {{ $round->end_date }}">
-                                            <span class="label label-primary">Not registered yet</span>
+                                        <span data-toggle="tooltip" data-placement="top" title="Start: {{ $round->start_date }}.<br>Ended: {{ $round->end_date }}.<br>User registration date: {{ $user->created_at }}.">
+                                            <span class="label label-primary">User was not registered</span>
                                         </span>
                                     @elseif(!$round->hasEnded())
-                                        <span data-toggle="tooltip" data-placement="top" title="Start: {{ $round->start_date }}.">
-                                            <a href="{{ route('round.register', $round) }}" class="btn btn-success btn-round">REGISTER</a>
+                                        <span data-toggle="tooltip" data-placement="top" title="Join round {{ $round->number }}!">
+                                            <a href="{{ route('round.register', $round) }}" class="btn btn-success btn-round"><i class="fas fa-plus-circle"></i> Register</a>
                                         </span>
+                                        <p>
+                                        @if(!$round->hasStarted())
+                                            <small style="text-muted">The round starts at {{ $round->start_date }}. The target land size is {{ number_format($round->land_target) }} acres.</p>
+                                        @else
+                                            <small style="text-muted">The round started at {{ $round->start_date }}. Current tick: {{ number_format($round->ticks) }}.</p>
+                                        @endif
+                                        </p>
                                     @endif
                                 @endif
                             </td>
@@ -140,207 +133,16 @@
                 </table>
             </div>
         </div>
-        {{--
-        <div class="col-lg-6">
-            <div class="box box-primary">
+        <div class="col-sm-12 col-md-3">
+            <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title"><i class="ra ra-capitol ra-fw"></i> Dominions</h3>
+                    <h3 class="box-title">Information</h3>
                 </div>
-
-                @if ($dominions->isEmpty())
-
-                    <div class="box-body">
-                        <p>Are you ready to join the fray? Click the Register button to join the current round.</p>
-                        @if ($discordInviteLink = config('app.discord_invite_link'))
-                            <p>If you need any help, come join us on Discord.</p>
-                            <p style="padding: 0 20px;">
-                                <a href="{{ $discordInviteLink }}" target="_blank">
-                                    <img src="{{ asset('assets/app/images/join-the-discord.png') }}" alt="Join the Discord" class="img-responsive">
-                                </a>
-                            </p>
-                        @endif
-                    </div>
-
-                @else
-
-                    <div class="box-body table-responsive no-padding">
-                        <table class="table">
-                            <colgroup>
-                                <col width="80">
-                                <col>
-                                <col width="200">
-                                <col width="80">
-                                <col width="80">
-                                <col width="80">
-                            </colgroup>
-                            <thead>
-                                <tr>
-                                    <th class="text-center">Round</th>
-                                    <th>Dominion</th>
-                                    <th class="text-center">Faction</th>
-                                    <th class="text-center">Land</th>
-                                    <th class="text-center">Networth</th>
-                                    <th class="text-center">Realm</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($dominions->all() as $dominion)
-                                    <tr>
-                                        <td class="text-center">
-                                            {{ $dominion->round->number }}
-                                        </td>
-                                        <td>
-                                            @if (!$dominion->round->hasStarted())
-                                                <span class="label label-success">Starting soon</span>
-                                            @endif
-
-                                            @if($dominion->isLocked())
-                                                <span class="label label-info">Finished</span>
-                                            @endif
-
-                                            @if ($dominion->isSelectedByAuthUser())
-                                                <a href="{{ route('dominion.status') }}">{{ $dominion->name }}</a>
-                                                <span class="label label-success">Selected</span>
-                                            @else
-                                                <form action="{{ route('dominion.select', $dominion) }}" method="post">
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-link" style="padding: 0;">{{ $dominion->name }}</button>
-                                                </form>
-                                            @endif
-
-                                            @if($dominion->round->hasStarted() and !$dominion->isLocked())
-                                                <div class="col-sm-6 pull-right">
-                                                    <p>If you wish to abandon your dominion {{ $dominion->name }} in round {{ $dominion->round->number }}, you can do so here by checking confirm and then pressing the button. <em>This action cannot be undone.</em></p>
-                                                    <form action="{{ route('dominion.abandon', $dominion) }}" method="post">
-                                                        @csrf
-                                                        <label>
-                                                            <input type="checkbox" name="remember" required> Confirm abandon
-                                                        </label>
-                                                        <button type="submit" class="btn btn-danger btn-xs">Abandon dominion</button>
-                                                    </form>
-                                                </div>
-                                            @endif
-                                        </td>
-                                        <td class="text-center">
-                                            {{ $dominion->race->name }}
-                                        </td>
-                                        <td class="text-center">
-                                            {{ number_format($landCalculator->getTotalLand($dominion)) }}
-                                        </td>
-                                        <td class="text-center">
-                                            {{ number_format($networthCalculator->getDominionNetworth($dominion)) }}
-                                        </td>
-                                        <td class="text-center">
-                                            #{{ $dominion->realm->number }}: {{ $dominion->realm->name }}
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-
-                @endif
-
+                <div class="box-body">
+                    <p>Welcome {{ $dominions->isEmpty() ? '' : 'back' }} to ODARENA, <strong>{{ Auth::user()->display_name }}</strong>!</p>
+                    <p>You have been playing since {{ $user->created_at }} and have participated in {{ number_format($dominions->count()) }} of {{ number_format($rounds->where('start_date', '>=', $user->created_at)->count()) }} rounds since joining.</p>
+                </div>
             </div>
         </div>
-
-        <div class="col-lg-6">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title"><i class="fa fa-clock-o fa-fw"></i> Rounds</h3>
-                </div>
-
-                @if ($rounds->isEmpty())
-
-                    <div class="box-body">
-                        <p>There are currently no active rounds.</p>
-                    </div>
-
-                @else
-
-                    <div class="box-body table-responsive no-padding">
-                        <table class="table">
-                            <colgroup>
-                                <col width="80">
-                                <col>
-                                <col width="160">
-                                <col width="80">
-                            </colgroup>
-                            <thead>
-                                <tr>
-                                    <th class="text-center">Round</th>
-                                    <th>Chapter</th>
-                                    <th>Era</th>
-                                    <th class="text-center">Status</th>
-                                    <th class="text-center">Register</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($rounds->all() as $round)
-                                    @php
-                                        $trClass = 'danger';
-                                        $userAlreadyRegistered = $round->userAlreadyRegistered(Auth::user());
-
-                                        if ($round->hasEnded()) {
-                                            $trClass = '';
-                                        } elseif ($userAlreadyRegistered) {
-                                            $trClass = 'info';
-                                        } elseif ($round->hasStarted()) {
-                                            $trClass = 'warning';
-                                        } elseif ($round->openForRegistration()) {
-                                            $trClass = 'success';
-                                        }
-                                    @endphp
-
-                                    <tr class="{{ $trClass }}">
-                                        <td class="text-center">{{ $round->number }}</td>
-                                        <td>
-                                            {{ $round->name }}
-                                        </td>
-                                        <td>
-                                            {{ $round->league->description }}
-                                        </td>
-                                        <td class="text-center">
-                                            @if ($round->hasEnded())
-                                                <abbr title="Ended at {{ $round->end_date }}">Ended</abbr>
-                                            @elseif ($round->isActive() and $round->hasCountdown())
-                                                <abbr title="Ending at {{ $round->end_date }}">
-                                                    Ending in {{ $round->hoursUntilEnd() }} {{ str_plural('hour', $round->hoursUntilEnd()) }}
-                                                </abbr>
-                                            @elseif ($round->hasStarted() and !$round->hasCountdown())
-                                                <abbr title="Started at {{ $round->start_date }}">
-                                                    Started {{ number_format($round->ticks) }} {{ str_plural('tick', $round->ticks) }} ago
-                                                </abbr>
-                                            @else
-                                                <abbr title="Starting at {{ $round->start_date }}">
-                                                    Starting in <strong>{{ $round->hoursUntilStart() }}</strong> {{ str_plural('hours', $round->daysUntilStart()) }}
-                                                </abbr>
-                                            @endif
-                                        </td>
-                                        <td class="text-center">
-                                            @if ($round->hasEnded())
-                                                <a href="{{ route('chronicles.round', $round) }}">Rankings</a>
-                                            @elseif ($userAlreadyRegistered && $round->isActive())
-                                                Playing
-                                            @elseif ($userAlreadyRegistered && !$round->hasStarted())
-                                                Registered
-                                            @elseif ($round->openForRegistration())
-                                                <a href="{{ route('round.register', $round) }}" class="btn btn-primary btn-flat btn-xs">REGISTER</a>
-                                            @else
-                                                In {{ $round->daysUntilRegistration() }} day(s)
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-
-                @endif
-
-            </div>
-        </div>
-        --}}
-
     </div>
 @endsection
