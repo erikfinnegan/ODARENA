b38a20efe05842382d8dc91dde047fdb2ad7a67d
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2022-02-02 19:15:48 +0200

    R66

--- src/Helpers/UnitHelper.php
+++ src/Helpers/UnitHelper.php
@@ -215,7 +215,10 @@ class UnitHelper
             'casualties_on_offense' => '%s%% casualties on offense.',
 
             'casualties_from_wizard_ratio' => '%s%% * Wizard Ratio casualties.',
-            'immortal_from_wpa' => 'Immortal if Wizard Ratio is at least %s on offense (if invading)  or on defense (if defending).',
+            'immortal_from_wpa' => 'Immortal if Wizard Ratio is at least %s on offense (if invading) or on defense (if defending).',
+
+            'casualties_from_spy_ratio' => '%s%% * Spy Ratio casualties.',
+            'immortal_from_spa' => 'Immortal if Spy Ratio is at least %s on offense (if invading) or on defense (if defending).',
 
             'casualties_on_victory' => '%s%% casualties when successfully invading.',
             'casualties_on_fending_off' => '%s%% casualties when successfully fending off an invasion.',
