900dbdbd8530b1de421b61719a1f00d675b18112
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2022-02-25 18:56:36 +0200

    R68

--- app/resources/views/pages/dominion/debug.blade.php
+++ app/resources/views/pages/dominion/debug.blade.php
@@ -172,19 +172,6 @@
         </div>
         <div class="col-sm-12 col-md-4">
 
-            {{--<div class="box">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Banking Calculator</h3>
-                </div>
-                <div class="box-body">
-                    <p>
-                        {!! \OpenDominion\Http\Controllers\DebugController::printMethodValues($bankingCalculator, [
-                            'getResources',
-                        ]) !!}
-                    </p>
-                </div>
-            </div>--}}
-
             <div class="box">
                 <div class="box-header with-border">
                     <h3 class="box-title">Construction Calculator</h3>
