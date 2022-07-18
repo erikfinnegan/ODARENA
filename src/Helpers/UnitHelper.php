b19178d6b0a847ea20b324a5a93d5257656cbf91
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2022-07-18 06:40:34 +0300

    Update UnitHelper.php

--- src/Helpers/UnitHelper.php
+++ src/Helpers/UnitHelper.php
@@ -543,7 +543,7 @@ class UnitHelper
 
                     $perkValue[1] = number_format($perkValue[1], 2);
 
-                    $perkValue = []
+                    #$perkValue = []
                 }
 
 
