e0542f79c9cc3587d1ae01644f483d82864875e0
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2022-08-24 06:32:45 +0300

    Update watched-dominions.blade.php

--- app/resources/views/pages/dominion/insight/watched-dominions.blade.php
+++ app/resources/views/pages/dominion/insight/watched-dominions.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm-12 col-md-9">
+        <div class="col-sm-12 col-md-12">
             <div class="box box-primary">
                 <div class="box-header">
                     <h3 class="box-title"><i class="fas fa-eye"></i> Watched Dominions</h3>
@@ -122,6 +122,7 @@
             </div>
         </div>
 
+        {{-- 
         <div class="col-sm-12 col-md-3">
             <div class="box">
                 <div class="box-header with-border">
@@ -132,6 +133,7 @@
                 </div>
             </div>
         </div>
+        --}}
 
     </div>
 @endsection
